93930068509b9784abdfde1eca69e74a2b568fc8
Ted Mielczarek <ted@mielczarek.org>
2012-11-30 14:09:59 -0500

    measure FPS over entire sample period, also provide all instantaneous FPS estimates

--- b2gperf.py
+++ b2gperf.py
@@ -9,15 +9,22 @@ import sys, os
 from marionette import Marionette
 
 def measure_app_perf(marionette, gaia_path, app_name):
+    # Enable FPS counter first so data is stable by the time we measure it.
+    marionette.set_context(marionette.CONTEXT_CHROME)
+    script_dir = os.path.dirname(__file__)
+    marionette.import_script(os.path.join(script_dir, "fps.js"))
+    marionette.execute_script("""
+Components.utils.import("resource://gre/modules/Services.jsm");
+Services.prefs.setBoolPref("layers.acceleration.draw-fps", true);
+""")
     # Give apps a long timeout to launch
     marionette.set_script_timeout(60000)
     marionette.set_context(marionette.CONTEXT_CONTENT)
     marionette.import_script(os.path.join(gaia_path, "tests/atoms/gaia_apps.js"))
     marionette.execute_script("GaiaApps.killAll()")
-    #XXX: fixme: do something better
+    #XXX: fixme: do something better?
     marionette.execute_script("navigator.mozSettings.createLock().set({'lockscreen.enabled': false});")
     #XXX: send home button press?
-    script_dir = os.path.dirname(__file__)
     marionette.import_script(os.path.join(script_dir, "launchapp.js"))
     res = marionette.execute_async_script("launch_app('%s')" % app_name)
     if not res:
@@ -26,17 +33,14 @@ def measure_app_perf(marionette, gaia_path, app_name):
     print "time_to_paint: %f" % res.get('time_to_paint')
     # try to get FPS
     marionette.set_context(marionette.CONTEXT_CHROME)
-    marionette.import_script(os.path.join(script_dir, "fps.js"))
-    marionette.execute_script("""
-Components.utils.import("resource://gre/modules/Services.jsm");
-Services.prefs.setBoolPref("layers.acceleration.draw-fps", true);
-""")
     period = 5000 # ms
+    sample_hz = 10
     marionette.set_script_timeout(period + 1000)
     try:
         #TODO: do something useful with all this data
-        fps = marionette.execute_async_script("measure_fps(%d)" % period)
-        print fps[-1];
+        fps = marionette.execute_async_script("measure_fps(%d,%d)" % (period, sample_hz))
+        print "FPS: %f/%f" % (fps.get('composition_fps'),
+                              fps.get('transaction_fps'))
     finally:
         marionette.execute_script("""Services.prefs.setBoolPref("layers.acceleration.draw-fps", false);""")
     marionette.set_context(marionette.CONTEXT_CONTENT)
