63597ddacd2026dae6a69fc7cd26feaf71f8d057
Dave Hunt <dave.hunt@gmail.com>
2012-12-07 12:56:11 +0000

    Use Gaia atoms path so we can use either gaia or gaia-ui-tests repositories, and enhance launch method with code from atoms

--- b2gperf.py
+++ b2gperf.py
@@ -3,12 +3,12 @@
 # To run this:
 # 1) Install a B2G build with Marionette enabled
 # 2) adb forward tcp:2828 tcp:2828
-# 3) b2gperf.py /path/to/gaia <app name>
+# 3) b2gperf.py /path/to/gaia/atoms <app name>
 
 import sys, os
 from marionette import Marionette
 
-def measure_app_perf(marionette, gaia_path, app_name):
+def measure_app_perf(marionette, gaia_atoms_path, app_name):
     # Enable FPS counter first so data is stable by the time we measure it.
     marionette.set_context(marionette.CONTEXT_CHROME)
     script_dir = os.path.dirname(__file__)
@@ -20,10 +20,10 @@ Services.prefs.setBoolPref("layers.acceleration.draw-fps", true);
     # Give apps a long timeout to launch
     marionette.set_script_timeout(60000)
     marionette.set_context(marionette.CONTEXT_CONTENT)
-    marionette.import_script(os.path.join(gaia_path, "tests/atoms/gaia_apps.js"))
+    marionette.import_script(os.path.join(gaia_atoms_path, "gaia_apps.js"))
     marionette.execute_async_script("GaiaApps.killAll()")
     # Unlock
-    marionette.import_script(os.path.join(gaia_path, "tests/atoms/gaia_lock_screen.js"))
+    marionette.import_script(os.path.join(gaia_atoms_path, "gaia_lock_screen.js"))
     marionette.execute_async_script("GaiaLockScreen.unlock()")
     # Return to home screen
     marionette.execute_script("window.wrappedJSObject.dispatchEvent(new Event('home'));")
@@ -45,17 +45,17 @@ Services.prefs.setBoolPref("layers.acceleration.draw-fps", true);
                               fps.get('transaction_fps'))
     marionette.execute_script("""Services.prefs.setBoolPref("layers.acceleration.draw-fps", false);""")
     marionette.set_context(marionette.CONTEXT_CONTENT)
-    marionette.execute_script("window.wrappedJSObject.WindowManager.kill('%s')" % res.get('origin'))
+    marionette.execute_async_script("GaiaApps.kill('%s')" % res.get('origin'))
 
 def main(args):
     if len(args) < 1:
-        print >>sys.stderr, "Usage: b2gperf.py <path to gaia> [app name]"
+        print >>sys.stderr, "Usage: b2gperf.py <path to gaia atoms> [app name]"
         sys.exit(1)
-    gaia_path = args[0]
+    gaia_atoms_path = args[0]
     app_name = args[1] if len(args) == 2 else "Clock"
     marionette = Marionette(host='localhost', port=2828)
     marionette.start_session()
-    measure_app_perf(marionette, gaia_path, app_name)
+    measure_app_perf(marionette, gaia_atoms_path, app_name)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- launchapp.js
+++ launchapp.js
@@ -1,23 +1,54 @@
 function launch_app(app_name) {
-  let start = null;
-  let origin = null;
-  let windows = document.getElementById("windows");
-  function firstpaint(evt) {
-    let end = performance.now();
-    windows.removeEventListener('mozbrowserfirstpaint', firstpaint);
-    //window.wrappedJSObject.WindowManager.getAppFrame(origin)
-    let id = evt.target.id;
-    marionetteScriptFinished({'frame': id, 'origin': origin, 'time_to_paint': end - start});
-  }
-
   GaiaApps.locateWithName(app_name, function(app, name, entry) {
-    if (app == false) {
-      marionetteScriptFinished(null);
-      return;
+    let start = null;
+    if (app) {
+      let runningApps = window.wrappedJSObject.WindowManager.getRunningApps();
+      let origin = GaiaApps.getRunningAppOrigin(name);
+      let alreadyRunning = !!origin;
+
+      start = performance.now();
+      app.launch(entry || null);
+
+      function sendResponse(origin, end) {
+        let app = runningApps[origin];
+        marionetteScriptFinished({frame: app.frame.id,
+          src: app.frame.src,
+          name: app.name,
+          origin: origin,
+          time_to_paint: end - start});
+      }
+
+      waitFor(
+        function() {
+          if (alreadyRunning) {
+            // return the app's frame id
+            sendResponse(origin);
+          }
+          else {
+            // wait until the new iframe sends the mozbrowserfirstpaint event
+            let frame = runningApps[origin].frame;
+            if (frame.dataset.unpainted) {
+              window.addEventListener('mozbrowserfirstpaint',
+                function firstpaint() {
+                  let end = performance.now();
+                  window.removeEventListener('mozbrowserfirstpaint',
+                    firstpaint);
+                  sendResponse(origin, end);
+                });
+            }
+            else {
+              sendResponse(origin);
+            }
+          }
+        },
+        // wait until the app is found in the running apps list
+        function() {
+          origin = GaiaApps.getRunningAppOrigin(name);
+          return !!origin;
+        }
+      );
+    } else {
+      marionetteScriptFinished(false);
     }
-    windows.addEventListener('mozbrowserfirstpaint', firstpaint);
-    origin = app.origin;
-    start = performance.now();
-    app.launch(entry || null);
   });
 }
