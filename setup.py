55a5794b9499a1346f567d5447334f7b61dcd70e
Jonathan Griffin <jgriffin@mozilla.com>
2013-01-30 15:40:32 -0800

    Bug 836210 - Make b2gperf a Python package, r=dhunt

---
+++ setup.py
@@ -0,0 +1,39 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+# get documentation from the README
+try:
+    here = os.path.dirname(os.path.abspath(__file__))
+    description = file(os.path.join(here, 'README.md')).read()
+except (OSError, IOError):
+    description = ''
+
+# dependencies
+deps = ['gaiatest==0.5',
+        'datazilla>=1.2',
+        'mozdevice>=0.19']
+
+setup(name='b2gperf',
+      version=version,
+      description="App startup tests for B2G",
+      long_description=description,
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='mozilla',
+      author='Mozilla Automation and Testing Team',
+      author_email='tools@lists.mozilla.org',
+      license='MPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points="""
+      [console_scripts]
+      b2gperf = b2gperf.b2gperf:cli
+      """,
+      install_requires=deps,
+      )
