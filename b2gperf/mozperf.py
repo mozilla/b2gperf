47bfea82beb6b5b21bbf81dbc3f7cc20e1c4be9f
Jonathan Griffin <jgriffin@mozilla.com>
2013-02-20 15:03:47 -0800

    Add script to process mozPerfDuration results

---
+++ b2gperf/mozperf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+
+import json
+
+from b2gperf import dzOptionParser, DatazillaPerfPoster
+from marionette import Marionette
+
+
+class MozPerfHandler(DatazillaPerfPoster):
+
+    def process_results(self, filename):
+        with open(filename, 'r') as f:
+            raw_results = json.loads(f.read())
+
+        for app_results in raw_results:
+            results = {'mozPerfDuration': []}
+            app_name = app_results.get('stats', {}).get('application')
+            if not len(app_results.get('passes', ())):
+                print "no passing results for %s, skipping" % app_name
+            else:
+                for result in app_results.get('passes'):
+                    results['mozPerfDuration'].extend(result.get('mozPerfDurations'))
+            if self.submit_report:
+                self.post_to_datazilla(results, app_name)
+            else:
+                print 'results for %s' % app_name
+                print json.dumps(results)
+
+if __name__ == "__main__":
+    parser = dzOptionParser(usage='%prog [options] result_file')
+    options, args = parser.parse_args()
+
+    if not args:
+        parser.print_usage()
+        parser.exit()
+
+    if len(args) != 1:
+        parser.exit("You can only specify one result file")
+
+    datazilla_config = parser.datazilla_config(options)
+
+    marionette = Marionette(host='localhost', port=2828)
+    marionette.start_session()
+    handler = MozPerfHandler(marionette, datazilla_config=datazilla_config)
+    handler.process_results(args[0])
